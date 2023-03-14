fb7a68117de5e60e52ca80885365d580263a1723
Zekmos Code Hub <117698974+zekmoscodehub@users.noreply.github.com>
2023-03-14 02:49:23 +0000

    form side

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-document.querySelectorAll('.form-outline').forEach((formOutline) => {
-    new mdb.Input(formOutline).init();
-  });
+const form = document.querySelector('form');
+
+//attach an event listener to the form submit event
+form.addEventListener('submit', (event) => {
+  //prevent the default form submission
+  event.preventDefault();
+
+  //get the form data
+  const formData = new FormData(form);
+
+  //create a new XMLHttpRequest object
+  const xhr = new XMLHttpRequest();
+
+  //set the request method and URL
+  xhr.open('POST', 'https://zekmoscodehub.org/send-contact-form');
+
+  //set the request headers
+  xhr.setRequestHeader('Content-Type', 'application/json');
+
+  //convert the form data to a JSON string
+  const jsonData = JSON.stringify(Object.fromEntries(formData.entries()));
+
+  //send the request
+  xhr.send(jsonData);
+  
+  //reset the form
+  form.reset();
+});
+
+
+
+
+  //get the form element
+
