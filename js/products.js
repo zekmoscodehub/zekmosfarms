3a17cacd347329db90ace709ed653f85cfe5facc
Zekmos Code Hub <117698974+zekmoscodehub@users.noreply.github.com>
2024-09-05 05:37:01 +0000

    added products
    eggs and term

--- js/products.js
+++ js/products.js
@@ -10,7 +10,7 @@ const products = [
 
     {
         id: 2,
-        name: 'Day Old Chick',
+        name: 'Day Old Chicks',
         price:'21',
         description: 'Healthy day-old chicks ready for your farm.',
         image: 'img/chicks.jpg'
@@ -22,26 +22,32 @@ const products = [
         description: 'Vibrant guinea keets for your farm.',
         image: 'img/gc7.jpeg'
     },
-    
     {
         id: 4,
-        name: 'Full-grown Guinea Fowl',
-        price: '120',
-        description: 'Full-grown guinea fowls for meat or breeding.',
-        image: 'guineas-fowl.jpg'
+        name: 'Fertile Guinea fowl eggs',
+        price: '5',
+        description: 'Our Fertile eggs are collected from our Healthy and well nurished breeds,They are perfect for hatching and good for consuption.',
+        image: 'img/Guinea-Keet-Eggs.jpg'
     },
     {
         id: 5,
-        name: 'Incubator (Medium)',
+        name: 'Full-grown Guinea Fowl',
+        price: '120',
+        description: 'Full-grown guinea fowls, best for meat or breeding.',
+        image: 'guineas-fowl.jpg'
+    },
+    {
+        id: 6,
+        name: 'Incubator (Medium TL210)',
         price: '2500',
         description: 'Medium capacity Automatic Programmable incubator, perfect for small farms.',
         image: 'img/incu5.jpeg'
     },
     {
-        id: 6,
+        id: 7,
         name: 'Brooding Lamp',
         price: '150',
-        description: '120 Wats, Brooding lamp to keep your chicks warm.',
+        description: '150 Wats, Brooding lamp to keep your chicks warm.',
         image: 'bro.jpeg'
     }
 ];
